0a0c16186bf26a591aea593082a01172e048b307
Pablo Ramirez <pablillo.es@gmail.com>
2014-01-24 15:03:42 +0100

    ajax puntos

--- php/cliente/points/pointDomain.js
+++ php/cliente/points/pointDomain.js
@@ -1,16 +1,14 @@
 (function() {
     var root = this;
     
-    function getPoint(point_id){
+    function getPoint(point_id, callback){
         $.ajax({
                 type: 'POST',
                 url: '../../servidor/services/pointService.php/getPoint/',
                 dataType: 'json',
                 data:{point_id:point_id}
         }).done(function(data){
-            console.log(data); 
-            MAP_APP.renderPoint(data);
-                       
+            callback(data);
         }).fail(function() {                                    
             console.log("error");            
         });        
--- php/cliente/points/pointInit.js
+++ php/cliente/points/pointInit.js
@@ -1,32 +1,32 @@
-(function (){
+(function() {
     var root = this;
-    $(document).ready(function(){
+    $(document).ready(function() {
         //RECUPERAR PUNTO
-        $('#get-point').on('click',function(){
+        $('#get-point').on('click', function() {
             var point_id = $('#point-id').val();
             viewPointDOM(point_id);
-        });	
-     });  
-     
-     function viewPointDOM(point_id){
-         MAP_APP.getPoint(point_id)         
-     }
-     function renderPoint(data){         
-         var result =""; 
-         result += "<br/>Estado:"+data.status;
-         result += "<br/>Mensaje:"+data.message;
-         result += "<br/>Id:"+data.result.point_id;
-         result += "<br/>CoordX:"+data.result.point_x;
-         result += "<br/>CoordY:"+data.result.point_y;
-         $('#result').html(result); 
-     }
-	
-		
+        });
+    });
+
+    function viewPointDOM(point_id) {
+        MAP_APP.getPoint(point_id, function(data) {
+            var result = "";
+            result += "<br/>Estado:" + data.status;
+            result += "<br/>Mensaje:" + data.message;
+            result += "<br/>Id:" + data.result.point_id;
+            result += "<br/>CoordX:" + data.result.point_x;
+            result += "<br/>CoordY:" + data.result.point_y;
+            $('#result').html(result);
+        });
+
+    }
+    
+
+
     if (!root.MAP_APP) {
         root.MAP_APP = {};
         root.MAP_APP = {};
     }
     root.MAP_APP.viewPointDOM = viewPointDOM;
-    root.MAP_APP.renderPoint = renderPoint;
-    
+
 }).call(this);
