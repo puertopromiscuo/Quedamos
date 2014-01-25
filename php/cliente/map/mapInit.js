9c77b39b433a5be2a840ced0c20947479e0297d0
psramirez <pablillo.es@gmail.com>
2014-01-25 16:54:39 +0100

    editando marcadores

--- php/cliente/map/mapInit.js
+++ php/cliente/map/mapInit.js
@@ -1,14 +1,14 @@
 (function() {
     var root = this;
     $(document).ready(function() {
-      initialize()
-      getAllEvents(); 
-      
+        initialize()
+        getAllEvents();
+
     });
-    
+
     var map;
-    var markers = [];   
-    
+    var markers = [];
+
     //Cargar mapa
     function initialize() {
         var center = new google.maps.LatLng(52.520816, 13.410186);
@@ -20,27 +20,54 @@
         map = new google.maps.Map(document.getElementById('map-canvas'),
                 mapOptions);
     }
-    
+
     //Obtiene todos los puntos en un array
     function getAllEvents() {
         MAP_APP.getAllPoints(function(data) {
-            //markers = data.slice(0);//clona array y lo copia  
-            var point_x;
-            var point_y;
+            //markers = data.slice(0);//clona array y lo copia              
             for (var i = 0; i < data.length; i++) {
-                addMarker(data[i].point_id,data[i].point_x,data[i].point_y);
-            }            
+                addMarker(data[i].point_id, data[i].point_x, data[i].point_y);
+            }
         });
     }
-    
-    function addMarker(point_id,point_x,point_y) {
+
+    function addMarker(point_id, point_x, point_y) {
         var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(point_x,point_y),
+            position: new google.maps.LatLng(point_x, point_y),
             map: map,
+            title: 'Click to zoom',
             animation: google.maps.Animation.DROP,
-            marker_id:point_id
+            marker_id: point_id
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            map.setZoom(15);
+            map.setCenter(marker.getPosition());
+            animaMarker(this);
+            console.log(this.marker_id);
+
         });
         markers.push(marker);
+    }
+    
+    //Animar marcador onclick
+    function animaMarker(marker) {
+        if (marker.getAnimation() != null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
+
+    //Oculta marcadores del mapa
+    function clearMarkers() {
+        setAllMap(null);
+        console.log("clear");
+    }
+
+    // Mostrar los marcadores del array
+    function showMarkers() {
+        setAllMap(map);
+        console.log("show");
     }
     //Muestra y Oculta marcadores en el mapa
     function setAllMap(map) {
@@ -48,19 +75,24 @@
             markers[i].setMap(map);
         }
     }
-    
+
 
 
     if (!root.MAP_APP) {
-        root.MAP_APP = {};       
-    }    
+        root.MAP_APP = {};
+    }
+    root.MAP_APP.getAllEvents = getAllEvents;
+    root.MAP_APP.getAllEvents = getAllEvents;
+    root.MAP_APP.getAllEvents = getAllEvents;
+    root.MAP_APP.getAllEvents = getAllEvents;
+    root.MAP_APP.getAllEvents = getAllEvents;
     root.MAP_APP.getAllEvents = getAllEvents;
 
 }).call(this);
 /*
  * function getPointDOM(point_id) {
-        MAP_APP.getPoint(point_id, function(data) {
-            console.log(data);
-        });
-    }
+ MAP_APP.getPoint(point_id, function(data) {
+ console.log(data);
+ });
+ }
  */
