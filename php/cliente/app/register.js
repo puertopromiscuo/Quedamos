3e1c3982151eb41f92a8467713b743ee7ef6b44b
jeiker26 <jesusgraficap@gmail.com>
2014-02-11 18:33:17 +0100

    desbloqueando mapa

--- php/cliente/app/register.js
+++ php/cliente/app/register.js
@@ -35,93 +35,95 @@ iris.ui(
             self.destroy = function() {
                 console.log("register UI Destroyed");
             };
+
+            function sendRegister() {
+                var formRegister = $('#register-content');
+                $.ajax({
+                    type: 'POST',
+                    url: 'servidor/services/loginService/insertUser',
+                    dataType: 'json',
+                    data: {
+                        name: formRegister.find('input[name = user-name]').val(),
+                        email: formRegister.find('input[name = user-email]').val(),
+                        password: formRegister.find('input[name = user-password]').val()}
+                }).done(function(data) {
+                    viewRegister(data);
+                    console.log(data);
+                }).fail(function() {
+                    console.log(arguments);
+                });
+            }
+
+            function logSend() {
+                var formLogin = $('#login-content');
+                $.ajax({
+                    type: 'POST',
+                    url: 'servidor/services/loginService/logUser',
+                    dataType: 'json',
+                    data: {
+                        email: formLogin.find('input[name = user-email]').val(),
+                        password: formLogin.find('input[name = user-password]').val()}
+                }).done(function(data) {
+                    renderUser(data);
+                }).fail(function() {
+                    console.log(arguments);
+                });
+            }
+
+            function forgetPass() {
+                var formForget = $('#forget-content');
+                $.ajax({
+                    type: 'POST',
+                    url: 'servidor/services/loginService/forgetPass',
+                    dataType: 'json',
+                    data: {
+                        email: formForget.find('input[name = user-email]').val()}
+                }).done(function(data) {
+                    console.log(data);
+                    viewRegister(data);
+                }).fail(function() {
+                    console.log(arguments);
+                });
+            }
+
+            function viewFormForgetPass() {
+                $('#myTab').find('li[class=active]').removeClass('active');
+                $('#login-content').removeClass('active');
+                $('#register-content').removeClass('active');
+
+                $('#forget-content').addClass('active');
+            }
+
+            function viewRegister(dataRegister) {
+                if (dataRegister.status === "ok") {
+                    $('#ok-form').html(dataRegister.message);
+                    $('#ok-form').removeClass('hidden');
+                    $('#error-form').addClass('hidden');
+                    //Limpiar campos del formulario
+                    $(':input', '#register-content').val('');
+                } else if (dataRegister.status === "error") {
+                    $('#error-form').html(dataRegister.message);
+                    $('#error-form').removeClass('hidden');
+                } else {
+                    console.log("Error al registrar el usuario, intentelo más tarde.");
+                }
+            }
+
+            function renderUser(dataUser) {
+                if (dataUser.status == "error") {
+                    $('#error-form').html(dataUser.message);
+                    $('#error-form').removeClass('hidden');
+                } else {
+                    $('#close').click();
+                    iris.navigate("#/loged");
+                    var $userName = $('<p/>', {
+                        html: dataUser.result.name,
+                        href: '#user'
+                    });
+                    $userName.appendTo('#menu-top');
+                }
+            }
         },
         iris.path.ui.register.js
         );
 
-function sendRegister() {
-    var formRegister = $('#register-content');
-    $.ajax({
-        type: 'POST',
-        url: 'servidor/services/loginService/insertUser',
-        dataType: 'json',
-        data: {
-            name: formRegister.find('input[name = user-name]').val(),
-            email: formRegister.find('input[name = user-email]').val(),
-            password: formRegister.find('input[name = user-password]').val()}
-    }).done(function(data) {
-        viewRegister(data);
-        console.log(data);
-    }).fail(function() {
-        console.log(arguments);
-    });
-}
-
-function logSend() {
-    var formLogin = $('#login-content');
-    $.ajax({
-        type: 'POST',
-        url: 'servidor/services/loginService/logUser',
-        dataType: 'json',
-        data: {
-            email: formLogin.find('input[name = user-email]').val(),
-            password: formLogin.find('input[name = user-password]').val()}
-    }).done(function(data) {
-        renderUser(data);
-    }).fail(function() {
-        console.log(arguments);
-    });
-}
-
-function forgetPass() {
-        var formForget = $('#forget-content');
-        $.ajax({
-            type: 'POST',
-            url: 'servidor/services/loginService/forgetPass',
-            dataType: 'json',
-            data: {
-                email: formForget.find('input[name = user-email]').val()}
-        }).done(function(data) {
-            console.log(data);
-            viewRegister(data);
-        }).fail(function() {
-            console.log(arguments);
-        });
-    }
-
-function viewFormForgetPass() {
-    $('#myTab').find('li[class=active]').removeClass('active');
-    $('#login-content').removeClass('active');
-    $('#register-content').removeClass('active');
-
-    $('#forget-content').addClass('active');
-}
-
-function viewRegister(dataRegister) {
-    if (dataRegister.status === "ok") {
-        $('#ok-form').html(dataRegister.message);
-        $('#ok-form').removeClass('hidden');
-        $('#error-form').addClass('hidden');
-        //Limpiar campos del formulario
-        $(':input', '#register-content').val('');
-    } else if (dataRegister.status === "error") {
-        $('#error-form').html(dataRegister.message);
-        $('#error-form').removeClass('hidden');
-    } else {
-        console.log("Error al registrar el usuario, intentelo más tarde.");
-    }
-}
-
-function renderUser(dataUser) {
-    if (dataUser.status == "error") {
-        $('#error-form').html(dataUser.message);
-        $('#error-form').removeClass('hidden');
-    } else {
-        iris.navigate("#/loged");
-        var $userName = $('<p/>', {
-            html: dataUser.result.name,
-            href: '#user'
-        });
-        $userName.appendTo('#menu-top');
-    }
-}
