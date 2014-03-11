7a4d88daa55c63b8991a5a141845d545b0ea2167
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2014-03-11 11:02:11 +0100

    diseño

--- php/cliente/app/map/elements/myEventUI.js
+++ php/cliente/app/map/elements/myEventUI.js
@@ -2,25 +2,26 @@
     var root = this;
 
     function createMyEvent(event_id, event_title, event_date, event_users) {
-        container = $('<div data-eventid ="' + event_id + '">');
+        container = $('<div data-eventid ="' + event_id + '" class="row"><br>');
         
         del = $('<button id="del-my-event"  class="glyphicon glyphicon-remove btn btn-danger"> </button>');
         del.on("click", function() {
             console.log($(this).parent().parent().data("eventid"));
         })
-        divDel = $(' <div class="col-sm-2">').append(del);
-        container.append(divDel);
+        
 
-        title = $('<div class="col-sm-10"><span>' + event_title + '</span></div>');
+        title = $('<div class="col-xs-8"><span>' + event_title + '</span><br><span>' + event_date + '</span></div>');
         container.append(title);
 
-        date = $('<div class="col-sm-6"><span>' + event_date + '</span></div>');
-        container.append(date);
+        /*date = $('<div class="col-xs-3"><span>' + event_date + '</span></div>');
+        container.append(date);*/
         
-        /*users = $('<div class="col-sm-2"><span class="badge">' + event_users.length + '</span></div>');
-        container.append(users);*/   
+        users = $('<div class="col-xs-2"><span class="badge">' + event_users.length + '</span></div>');
+        container.append(users);  
 
-
+        divDel = $(' <div class="col-xs-2">').append(del);
+        container.append(divDel);
+        
         return container;
     }
 
