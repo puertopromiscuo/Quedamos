7e736741d28d722f2ef328bbccbb1b71f34f9c7b
jeiker26 <jesusgraficap@gmail.com>
2014-03-13 23:48:08 +0100

    lista casi acabado diseño

--- php/cliente/app/map/elements/myEventUI.js
+++ php/cliente/app/map/elements/myEventUI.js
@@ -2,16 +2,21 @@
     var root = this;
 
     function createMyEvent(event_id, event_title, event_date, event_users) {
-        container = $('<div data-eventid ="' + event_id + '" class="row"><br>');
+        li = $('<li data-eventid ="' + event_id + '" ><br></li>');
+        container = $('<div data-eventid ="' + event_id + '" class="row"><br></div>');
         
-        del = $('<button id="del-my-event"  class="glyphicon glyphicon-remove btn btn-danger"> </button>');
+        del = $('<button id="del-my-event" class="glyphicon glyphicon-remove btn btn-danger btn-sm"> </button>');
         del.on("click", function() {
             EVENTS.deleteEvent($(this).parent().parent().data("eventid"),function(data){
                 iris.notify("render",true);            
                 iris.notify("alertSuccess","Evento borrado");
             });            
         })
-        title = $('<button class="col-xs-8"><span>' + event_title + '</span><br><span>' + event_date + '</span></button>');
+        conf = $('<button id="conf-my-event" class="glyphicon glyphicon-edit btn btn-danger btn-sm"> </button>');
+        conf.on("click", function() {
+            console.log($(this).parent().parent().data("eventid"));
+        })
+        title = $('<button class="col-xs-4"><span>' + event_title + '</span></button>');
         title.on("click", function() {            
             for(i=0;i< MAP.markers.length;i++){
                 if(MAP.markers[i].event_id == event_id){                    
@@ -19,15 +24,22 @@
                 }
             }
         })
-        container.append(title);        
+        container.append(title);
+        date = $('<div class="col-xs-2"><small>' + event_date.substr(5,9).trim().replace("-","/") + '</small></div>');
+        container.append(date);        
         
         users = $('<div class="col-xs-2"><span class="badge">' + event_users.length + '</span></div>');
-        container.append(users);  
+        container.append(users);
+        
+        divConf = $('<div class="col-xs-2"></div>').append(conf);
+        container.append(divConf);
 
-        divDel = $(' <div class="col-xs-2">').append(del);
+        divDel = $('<div class="col-xs-2"></div>').append(del);
         container.append(divDel);
         
-        return container;
+        li.append(container);
+        
+        return li;
     }
 
 
